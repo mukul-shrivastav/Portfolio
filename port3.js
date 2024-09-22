daab70f05c0ff8c4561dcf8f9ae143b4d634b3ec
Mukul <mukulsrivastava1992@gmail.com>
2024-09-22 13:11:54 +0530

    portfolio

---
+++ port3.js
@@ -0,0 +1,7 @@
+var typed = new  typed (".multiple-text",  {
+    string : ["Frontend Developer " ,"Fitness Lover"],
+    typespeed:100,
+    backspeed:100,
+    backDealy:1000,
+    loop:true
+}) ;
